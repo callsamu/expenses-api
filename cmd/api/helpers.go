564598564e2e53bf86dc05770f22d7c525d0dbf3
callsamu <samuelbree@gmail.com>
2022-12-29 21:18:33 -0300

    implement writeJSON helper

---
+++ cmd/api/helpers.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
+}
