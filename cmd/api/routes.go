3f2eb66af61341d576e7d8377d37c7265818481e
callsamu <samuelbree@gmail.com>
2022-12-30 00:11:59 -0300

    add routes with httproute

---
+++ cmd/api/routes.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) routes() http.Handler {
+	r := httprouter.New()
+
+	r.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+
+	return r
+}
