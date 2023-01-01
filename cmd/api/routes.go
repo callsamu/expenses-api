37a4a4ff5eb2da21c2f80b16dde12807bf5d8930
callsamu <samuelbree@gmail.com>
2022-12-31 23:54:37 -0300

    add user registration to routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,6 +10,9 @@ func (app *application) routes() http.Handler {
 	r := httprouter.New()
 
 	r.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+
+	r.HandlerFunc(http.MethodPost, "/v1/users/register", app.registerUserHandler)
+
 	r.NotFound = http.HandlerFunc(app.notFoundResponse)
 
 	return r
---
+++ cmd/api/users.go
@@ -0,0 +1,7 @@
+package main
+
+import "net/http"
+
+func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
+
+}
