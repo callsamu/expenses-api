633ebfda407a9418fcab82ce199c405c8ec72963
callsamu <samuelbree@gmail.com>
2023-01-01 17:29:18 -0300

    implement readJSON helper with proper error handling

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -22,6 +25,34 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data interf
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write(js)
+
+	return nil
+}
+
+func (app *application) readJSON(r *http.Request, input any) error {
+	err := json.NewDecoder(r.Body).Decode(input)
+
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshallTypeError *json.UnmarshalTypeError
+		var invalidUnmarshallError *json.InvalidUnmarshalError
+
+		switch {
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+		case errors.As(err, &unmarshallTypeError):
+			if unmarshallTypeError.Field != "" {
+				return fmt.Errorf("body contains invalid JSON type for field %q", unmarshallTypeError.Field)
+			}
+			return fmt.Errorf("body contains invalid JSON type (at character %d)", unmarshallTypeError.Offset)
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+		case errors.As(err, &invalidUnmarshallError):
+			panic(err)
+		}
+	}
 
 	return nil
 }
