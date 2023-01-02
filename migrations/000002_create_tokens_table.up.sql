3ef01986afb5ece14b869077e4a6e8277060eb80
callsamu <samuelbree@gmail.com>
2023-01-01 21:52:35 -0300

    fix typos in migrations

--- migrations/000002_create_tokens_table.up.sql
+++ migrations/000002_create_tokens_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS tokens (
     hash bytea PRIMARY KEY,
     user_id bigint NOT NULL REFERENCES users ON DELETE CASCADE,
-    expiry timestamp(0) with timezone NOT NULL 
-    scope text NOT NULL,
+    expiry timestamp(0) with time zone NOT NULL,
+    scope text NOT NULL
 );
