2330f6b8c8c729ea20e4c19f47d14aa8969075ed
callsamu <samuelbree@gmail.com>
2023-01-03 15:40:03 -0300

    add edit conflict response

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -40,6 +40,11 @@ func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
 
+func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
+	message := "unable to update the record due to an edit conflict, please try again"
+	app.errorResponse(w, r, http.StatusConflict, message)
+}
+
 func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
