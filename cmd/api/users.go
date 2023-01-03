3532d3077f5da697700d34ada3bfd3f209319e69
callsamu <samuelbree@gmail.com>
2023-01-03 15:40:20 -0300

    implement activate users handler

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 
 	"github.com/callsamu/expenses-api/internal/data"
@@ -46,5 +47,58 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) activateUserHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Token string `json:"token"`
+	}
+	err := app.readJSON(r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+	if data.ValidateTokenPlaintext(v, input.Token); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	user, err := app.models.Users.GetForToken(data.ScopeActivation, input.Token)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			v.AddError("token", "invalid or expired activation token")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	user.Activated = true
+	err = app.models.Users.Update(user)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrEditConflict):
+			app.editConflictResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	err = app.models.Tokens.DeleteAllForUser(data.ScopeActivation, user.ID)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"user": user}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
 	}
 }
--- cmd/api/users_test.go
+++ cmd/api/users_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
 	"testing"
@@ -82,4 +83,76 @@ func TestRegisterUsersHandler(t *testing.T) {
 		assert.Equal(t, http.StatusBadRequest, response.StatusCode)
 	})
 
+}
+
+func TestActivateUsersHandler(t *testing.T) {
+	app, mock := newTestApplication(t)
+	tsrv := newTestServer(app.routes())
+
+	t.Run("activates user when token is found", func(t *testing.T) {
+		input := map[string]string{
+			"token": "abcdefghijklmnopqrstuvwxyz",
+		}
+
+		inputJSON, err := json.Marshal(input)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		columns := sqlmock.NewRows([]string{"id", "created_at", "name", "email", "password_hash", "activated", "version"})
+
+		password := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+		mock.ExpectQuery("SELECT").WillReturnRows(columns.AddRow(1, time.Now(), "foo", "foo@example.com", password, false, 1))
+		mock.ExpectQuery("UPDATE users").WillReturnRows(sqlmock.NewRows([]string{"version"}).AddRow(1))
+		mock.ExpectExec("DELETE FROM tokens").WillReturnResult(sqlmock.NewResult(1, 1))
+
+		t.Log(string(inputJSON))
+		response := tsrv.request(t, http.MethodPut, "/v1/users/activated", inputJSON)
+		require.Equal(t, http.StatusOK, response.StatusCode)
+
+		var output struct {
+			User data.User `json:"user"`
+		}
+		err = json.NewDecoder(response.Body).Decode(&output)
+
+		assert.EqualValues(t, 1, output.User.ID)
+		assert.True(t, output.User.Activated)
+
+		if err = mock.ExpectationsWereMet(); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("return validation error response when tokens is not found", func(t *testing.T) {
+		input := map[string]string{
+			"token": "abcdefghijklmnopqrstuvwxyz",
+		}
+
+		inputJSON, err := json.Marshal(input)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		mock.ExpectQuery("SELECT").WillReturnError(sql.ErrNoRows)
+		response := tsrv.request(t, http.MethodPut, "/v1/users/activated", inputJSON)
+		require.Equal(t, http.StatusUnprocessableEntity, response.StatusCode)
+		if err = mock.ExpectationsWereMet(); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("return validation error response when tokens is invalid", func(t *testing.T) {
+		input := map[string]string{
+			"token": "too short",
+		}
+
+		inputJSON, err := json.Marshal(input)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		response := tsrv.request(t, http.MethodPut, "/v1/users/activated", inputJSON)
+		require.Equal(t, http.StatusUnprocessableEntity, response.StatusCode)
+	})
 }
