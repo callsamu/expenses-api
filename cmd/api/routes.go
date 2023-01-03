8b47488725aac2694e8f4e131df86cd10bfb44e9
callsamu <samuelbree@gmail.com>
2023-01-03 15:41:24 -0300

    add activate user handler to routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -12,6 +12,7 @@ func (app *application) routes() http.Handler {
 	r.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
 	r.HandlerFunc(http.MethodPost, "/v1/users/register", app.registerUserHandler)
+	r.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
 	r.NotFound = http.HandlerFunc(app.notFoundResponse)
 
