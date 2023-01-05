36a5638d5c4d05d7b493ad056c6425f514e209dd
callsamu <samuelbree@gmail.com>
2023-01-04 23:51:33 -0300

    rewrite Models struct to use interfaces

--- internal/data/models.go
+++ internal/data/models.go
@@ -3,6 +3,7 @@ package data
 import (
 	"database/sql"
 	"errors"
+	"time"
 )
 
 var (
@@ -11,8 +12,17 @@ var (
 )
 
 type Models struct {
-	Users  UserModel
-	Tokens TokenModel
+	Users interface {
+		Insert(user *User) error
+		GetByEmail(email string) (*User, error)
+		Update(user *User) error
+		GetForToken(scope string, plaintext string) (*User, error)
+	}
+	Tokens interface {
+		New(userID int64, ttl time.Duration, scope string) (*Token, error)
+		Insert(token *Token) error
+		DeleteAllForUser(scope string, userID int64) error
+	}
 }
 
 func NewModels(db *sql.DB) Models {
