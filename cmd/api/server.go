8cae9d6e6003ff7daa42377facfb795503103b7a
callsamu <samuelbree@gmail.com>
2023-01-09 15:17:30 -0300

    handle graceful shutdowns

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func (app *application) serve() error {
-
 	srv := http.Server{
 		Handler:      app.routes(),
 		WriteTimeout: 30 * time.Second,
@@ -15,8 +19,33 @@ func (app *application) serve() error {
 		Addr:         fmt.Sprintf(":%d", app.config.port),
 	}
 
-	app.logger.Printf("starting %s server at port :%d", app.config.env, app.config.port)
-	err := srv.ListenAndServe()
+	shutdownError := make(chan error, 1)
 
-	return err
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		s := <-quit
+		app.logger.Printf("shutting down server (signal: %s)", s.String())
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		shutdownError <- srv.Shutdown(ctx)
+	}()
+
+	app.logger.Printf("starting %s server at port :%d", app.config.env, app.config.port)
+
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+
+	app.logger.Printf("stopped server at address %s", srv.Addr)
+	return nil
 }
