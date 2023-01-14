aef4b35f6dd5d140eed8b1f6591e56e46789e8cd
callsamu <samuelbree@gmail.com>
2023-01-14 17:37:08 -0300

    modify migration 000003 to include version column and a default for date

--- migrations/000003_create_expenses_table.up.sql
+++ migrations/000003_create_expenses_table.up.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS expenses (
     id bigserial PRIMARY KEY,
     user_id bigint NOT NULL REFERENCES users ON DELETE CASCADE,
-    date timestamp(0) with time zone NOT NULL,
+    date timestamp(0) with time zone NOT NULL DEFAULT NOW(), 
     recipient text NOT NULL,
     description text NOT NULL,
     amount bigint NOT NULL,
-    currency CHAR(3) NOT NULL
+    currency CHAR(3) NOT NULL,
+    version integer NOT NULL DEFAULT 1
 );
