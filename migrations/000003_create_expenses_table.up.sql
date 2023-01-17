7d5e3e3598e9f6000eb6d234cf421d3ed03c2fcd
callsamu <samuelbree@gmail.com>
2023-01-17 17:58:29 -0300

    fix: add category column for expenses table

--- migrations/000003_create_expenses_table.up.sql
+++ migrations/000003_create_expenses_table.up.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS expenses (
     date timestamp(0) with time zone NOT NULL DEFAULT NOW(), 
     recipient text NOT NULL,
     description text NOT NULL,
+    category text NOT NULL,
     amount bigint NOT NULL,
     currency CHAR(3) NOT NULL,
     version integer NOT NULL DEFAULT 1
